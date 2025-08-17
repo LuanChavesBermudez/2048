3e5c97d1aa8f40763fb06be2ea3b086b6de0b4d9
Luan Chaves <56284871+LuanChavesBermudez@users.noreply.github.com>
2025-08-17 16:44:23 -0600

    la interfaz se crea dinamicamente

--- 2048_script.js
+++ 2048_script.js
@@ -1,6 +1,6 @@
-var celdas = [];
 var movimentos = 0;
 var matriz;
+var areaJuego;
 
 window.onload = function() {
     inicio();
@@ -11,15 +11,18 @@ function inicio() {
         [0, 0, 0, 0],
         [0, 0, 0, 0],
         [0, 0, 0, 0],
-        [0, 0, 0, 0]
+        [0, 0, 0, 0]    
     ]
-
-    for (let i = 1; i < 17; i++) {
-        const celda = document.getElementById(i);
-        celdas.push(celda);
-    }
-    console.log(celdas);
-    for (let r = 0; r < 16; r++) {
-        celdas.at(r).textContent = "";
+    areaJuego = document.getElementById("AreaJuego");
+    for (let r=0; r < 4; r++) {
+        let fila = document.createElement("flex-container");
+        fila.classList.add("flex-container")
+        fila.id = r.toString();
+        areaJuego.appendChild(fila);
+        for (let c = 0; c < 4; c++) {
+            let columna = document.createElement("div");
+            columna.id = r.toString() + "-" + c.toString();
+            fila.appendChild(columna);
+        }
     }
 }
