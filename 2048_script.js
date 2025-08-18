af83d290592d5ccae3d14d9f719cd6977ef95f0d
Luan Chaves <56284871+LuanChavesBermudez@users.noreply.github.com>
2025-08-18 10:58:27 -0600

    genera valores aleatorios iniciales

--- 2048_script.js
+++ 2048_script.js
@@ -13,16 +13,41 @@ function inicio() {
         [0, 0, 0, 0],
         [0, 0, 0, 0]    
     ]
+
+    //genera dos columnas y filas aleatorias para valores iniciales
+    let indiceFila1 = Math.floor(Math.random() * 4);
+    let indiceFila2 = Math.floor(Math.random() * 4);
+    let indiceColumna1 = Math.floor(Math.random() * 4);
+    let indiceColumna2 = Math.floor(Math.random() * 4);
+
+    while (indiceFila1 == indiceFila2 && indiceColumna1 == indiceColumna2) {
+        let indiceFila2 = Math.floor(Math.random() * 4);
+        let indiceColumna2 = Math.floor(Math.random() * 4);
+    }
+
+    matriz[indiceFila1][indiceColumna1] = 2;
+    matriz[indiceFila2][indiceColumna2] = 2;
+
     areaJuego = document.getElementById("AreaJuego");
-    for (let r=0; r < 4; r++) {
+
+    //genera flex containers que funcionan como filas
+    for (let f=0; f < 4; f++) {
         let fila = document.createElement("flex-container");
         fila.classList.add("flex-container")
-        fila.id = r.toString();
+        fila.id = f.toString();
         areaJuego.appendChild(fila);
+
+        //genera divs dentro de los contenedores que funcionan como celdas o columnas
         for (let c = 0; c < 4; c++) {
-            let columna = document.createElement("div");
-            columna.id = r.toString() + "-" + c.toString();
-            fila.appendChild(columna);
+            let celda = document.createElement("div");
+            celda.id = f.toString() + "-" + c.toString();
+            celda.innerText = matriz[f][c];
+            if (celda.innerText == "0"){
+                celda.innerText = "";
+            }
+            fila.appendChild(celda);
         }
     }
 }
+
+//document.addEventListener("keyup", moverTablero);
