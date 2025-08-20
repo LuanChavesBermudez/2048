a5caca67814d54de0b47c9c02c5819d7af8f674a
Luan Chaves <56284871+LuanChavesBermudez@users.noreply.github.com>
2025-08-19 22:57:56 -0600

    funcion de movimiento a la izq

--- 2048_script.js
+++ 2048_script.js
@@ -20,7 +20,7 @@ function inicio() {
     let indiceColumna1 = Math.floor(Math.random() * 4);
     let indiceColumna2 = Math.floor(Math.random() * 4);
 
-    while (indiceFila1 == indiceFila2 && indiceColumna1 == indiceColumna2) {
+    if (indiceFila1 == indiceFila2 && indiceColumna1 == indiceColumna2) {
         let indiceFila2 = Math.floor(Math.random() * 4);
         let indiceColumna2 = Math.floor(Math.random() * 4);
     }
@@ -40,14 +40,60 @@ function inicio() {
         //genera divs dentro de los contenedores que funcionan como celdas o columnas
         for (let c = 0; c < 4; c++) {
             let celda = document.createElement("div");
+            //asigna una ID unica tipo fila-columna a cada celda para encontrarla facilmente
             celda.id = f.toString() + "-" + c.toString();
-            celda.innerText = matriz[f][c];
-            if (celda.innerText == "0"){
+            fila.appendChild(celda);
+        }
+    }
+
+    actualizarTablero();
+}
+
+function actualizarTablero(){
+    for (let f = 0; f < 4; f++) {
+        for (let c = 0; c < 4; c++) {
+            let celda = document.getElementById(f.toString() + "-" + c.toString());
+            let num = matriz[f][c];
+            if (num != 0) {
+                celda.innerText = num;
+            } else { 
                 celda.innerText = "";
             }
-            fila.appendChild(celda);
         }
     }
 }
 
-//document.addEventListener("keyup", moverTablero);
+
+function moverIzq() {
+    for (let f = 0; f < 4; f++) {
+        //variable para recordar el ultimo numero diferente a cero
+        let anterior = 0;
+        for (let c = 0; c < 4; c++) {
+            let actual = matriz[f][c];
+            //si el numero actual es diferente a cero
+            if (actual != 0) {
+                //si ha habido un numero previo diferente a cero y es igual al actual
+                if (anterior != 0 && anterior == actual) {
+                    //encuentra en que indice estaba ese valor previo
+                    let indiceAnterior = matriz[f].indexOf(anterior);
+                    matriz[f][indiceAnterior] = 0;
+                    matriz[f][c] = 0;
+                    //duplica el valor de esa celda y borra la actual (porque se fusionaron)
+                    let primerCero = matriz[f].indexOf(0);
+                    matriz[f][primerCero] = anterior*2;
+                    //olvida el numero previo para que no se fusionen muchos en cadena
+                    anterior = 0;
+                    continue;
+                }
+                //si no hay valor previo almacenado
+                matriz[f][c] = 0;
+                let primerCero = matriz[f].indexOf(0);
+                matriz[f][primerCero] = actual;
+                //lo mueve lo mas a la izquierda posible y lo guarda como el ultimo valor diferente a 0
+                anterior = actual;
+            }
+        }
+    }
+    //vuelve a dibujar los valores en pantalla
+    actualizarTablero();
+}
